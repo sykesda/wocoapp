ff6569d803dbbb26f5b73356b04741ea716daedd
castilloej <castilloej@email.wofford.edu>
2018-01-08 11:43:33 -0500

    Adding a comment to test

--- app/src/main/java/edu/wofford/sykesda/wocoapptest/MainActivity.java
+++ app/src/main/java/edu/wofford/sykesda/wocoapptest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // This is a comment
+        // This is a comment two
     }
 }
