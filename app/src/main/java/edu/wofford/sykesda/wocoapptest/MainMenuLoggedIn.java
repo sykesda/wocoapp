5b0988b7a2ad47c467598d0dbed347574831d0ab
castilloej <castilloej@email.wofford.edu>
2018-01-09 09:42:54 -0500

    Testing new branch ?

---
+++ app/src/main/java/edu/wofford/sykesda/wocoapptest/MainMenuLoggedIn.java
@@ -0,0 +1,13 @@
+package edu.wofford.sykesda.wocoapptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuLoggedIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_logged_in);
+    }
+}
