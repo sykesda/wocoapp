f01ce82291312a4d137dde8114876a7b4da5a3ed
castilloej <castilloej@email.wofford.edu>
2018-01-10 11:42:04 -0500

    Testing new branch ?

--- app/src/main/java/edu/wofford/sykesda/wocoapptest/MainMenuLoggedIn.java
+++ app/src/main/java/edu/wofford/sykesda/wocoapptest/MainMenuLoggedIn.java
@@ -1,7 +1,11 @@
 package edu.wofford.sykesda.wocoapptest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenuLoggedIn extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainMenuLoggedIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_logged_in);
+
+        final ImageButton openMap = (ImageButton) findViewById(R.id.mapActivityBttn);
+
+        openMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+
+    }
+
+    protected void openMap() {
+
+        Intent openMapsPlz = new Intent(this, MapsActivity.class);
+        startActivity(openMapsPlz);
     }
 }
