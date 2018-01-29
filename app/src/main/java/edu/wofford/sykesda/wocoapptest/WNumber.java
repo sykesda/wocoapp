b85cf01db31f81809f2eb221b9dbc14ae4b46678
andersonwr <35141647+andersonwr@users.noreply.github.com>
2018-01-29 14:06:50 -0500

    Modified WNumber.java and the respective XML layout to include a Delete W# button and a Manual Entry button. The Delete button currently works, but the Manual Entry does not.
    Added popup_wnumber_delete.xml to use as a popup confirmation when deleting.
    
    Corrected a small bug where the saved W# displayed a 9 instead of a W.

--- app/src/main/java/edu/wofford/sykesda/wocoapptest/WNumber.java
+++ app/src/main/java/edu/wofford/sykesda/wocoapptest/WNumber.java
@@ -3,10 +3,16 @@ package edu.wofford.sykesda.wocoapptest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,10 +20,13 @@ import android.widget.Toast;
 public class WNumber extends AppCompatActivity {
 
     Button scanIDButton;
+    Button deleteWNumButton;
+    Button manualEntryButton;
     TextView wNumText;
     String rawWnum;
-
-
+    ConstraintLayout constraintLayout;
+    PopupWindow popupWindow;
+    LayoutInflater layoutInflater;
     SharedPreferences sharedPref;
 
 
@@ -27,6 +36,9 @@ public class WNumber extends AppCompatActivity {
         setContentView(R.layout.activity_wnumber);
 
         scanIDButton = (Button) findViewById(R.id.scanIDButton);
+        deleteWNumButton = (Button) findViewById(R.id.clearWNumButton);
+        manualEntryButton = (Button) findViewById(R.id.manualEntryButton);
+
         wNumText = (TextView) findViewById(R.id.wNumText);
         sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
@@ -40,11 +52,62 @@ public class WNumber extends AppCompatActivity {
                 startScanMenu(v);
             }
         });
+
+        //Wire button to create popup for deleting saved W#
+        deleteWNumButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startDeleteConfirmPopup(v);
+            }
+        });
     }
 
     public void startScanMenu(View v){
         Intent intent = new Intent(this, WNumber_ScanMenu.class);
         startActivityForResult(intent, 0);
+    }
+
+    public void startDeleteConfirmPopup(View v){
+        constraintLayout = (ConstraintLayout) findViewById(R.id.wNumConstraint);
+
+        //Get screen metrics to center popup
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        //Get layoutInflater for creating popup
+        layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popup_wnumber_delete,null);
+
+        popupWindow = new PopupWindow(container, (int) (width*0.8), (int) (height*0.8), true);
+        popupWindow.showAtLocation(constraintLayout, Gravity.NO_GRAVITY, ((width/2) - (popupWindow.getWidth()/2)), ((height/2) - (popupWindow.getHeight()/2)));
+
+
+        Button deleteYes = (Button) popupWindow.getContentView().findViewById(R.id.delete_yes);
+        Button deleteNo = (Button) popupWindow.getContentView().findViewById(R.id.delete_no);
+
+        //Wire button to delete the saved WNumber
+        deleteYes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)  {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.remove("Wnum");
+                editor.commit();
+                wNumText.setText("No Saved W#");
+                popupWindow.dismiss();
+                Toast toast = Toast.makeText(getApplicationContext(), "W# deleted", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        //Wire button to close popup
+        deleteNo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)  {
+                popupWindow.dismiss();
+            }
+        });
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -55,7 +118,7 @@ public class WNumber extends AppCompatActivity {
                     wNumText.setText("W" + rawWnum.substring(1));
                     //save the Wnumber
                     SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("Wnum", rawWnum);
+                    editor.putString("Wnum", "W" + rawWnum.substring(1));
                     editor.commit();
                 }
             }
